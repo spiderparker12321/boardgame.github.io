93a58a57a5f7fa041d56130cc6096d57434ccad5
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-12 08:10:13 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,6 +4,6 @@ var responce;
 function hello() {
   if (responce = "parker")
   {
-  https://spiderparker12321.github.io/police-report-2004.github.io/stop;
+  window.location.replace("https://spiderparker12321.github.io/police-report-2004.github.io/stop");
   }
 }
