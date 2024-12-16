143581e88ea266204821fb0e03e733f2ba36e751
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-16 08:32:11 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,10 +1,11 @@
 var password = "disappearance.child.open";
 var responce;
 
-
+function hello()
+{
 while(responce != password){
 responce = window.prompt("enter password");
 window.location.replace("https://spiderparker12321.github.io/police-report-2004.github.io/stop");
 }
 window.location.replace("https://spiderparker12321.github.io/police-report-2004.github.io/stop");
-
+}
