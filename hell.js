0d7033cd36d6acfaf5335c6ef4e7c07d1959ac74
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-17 08:08:48 -0700

    Update hell.js

--- hell.js
+++ hell.js
@@ -7,3 +7,35 @@ var buttonClicked = false;
     console.log(buttonClicked);
   });
   });
+
+function desktopMove(e){
+
+if(buttonClicked == true){
+            var wH = $(window).height();
+            var wW = $(window).width();
+            var x = e.clientX;
+            var y = e.clientY;
+            if(x <= 20){
+                //Left
+                $pageTrigger = $('.pt-page-current').find('.right');
+                    if($pageTrigger.length)
+                        Animate($pageTrigger);
+            }else if(x >= (wW - 50)){
+                //Right
+                $pageTrigger = $('.pt-page-current').find('.left');
+                    if($pageTrigger.length)
+                        Animate($pageTrigger);
+            }else if(y <= 50){
+                //Top
+                $pageTrigger = $('.pt-page-current').find('.back');
+                    if($pageTrigger.length)
+                        Animate($pageTrigger);
+            }else if(y >= (wH - 50)){
+                //Bottom
+                $pageTrigger = $('.pt-page-current').find('.next');
+                    if($pageTrigger.length)
+                        Animate($pageTrigger);
+            }
+
+        }
+        }
